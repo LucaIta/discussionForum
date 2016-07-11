27c20c3d34bcf3cbbc7f7799a573d694d8925fd7
Luca Quatela and Noah Kittleson <noah@>
2016-07-11 16:48:58 -0700

    add list view for categories

--- app/src/main/java/com/epicodus/discussionforum/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/MainActivity.java
@@ -3,14 +3,29 @@ package com.epicodus.discussionforum;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.newCategoryButton) Button mNewCategoryButton;
+    private DatabaseReference mCategoryReference;
+    private ValueEventListener mCategoryReferenceEventListener;
+    private ArrayList<String> mCategoryList = new ArrayList<>();
+    @Bind(R.id.categoryListView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ButterKnife.bind(this);
         mNewCategoryButton.setOnClickListener(this);
 
+        mCategoryReference = FirebaseDatabase.getInstance().getReference().child(Constants.FIREBASE_CHILD_CATEGORY);
+
+        mCategoryReferenceEventListener = mCategoryReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot categorySnapshot : dataSnapshot.getChildren()) {
+                    mCategoryList.add(categorySnapshot.getValue().toString());
+                }
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mCategoryList);
+        mListView.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCategoryReference.removeEventListener(mCategoryReferenceEventListener);
     }
 
     @Override
--- app/src/main/java/com/epicodus/discussionforum/NewCategoryActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/NewCategoryActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -32,7 +31,6 @@ public class NewCategoryActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View view) {
         if (view == mNewCategoryButton) {
             String newCategory = mNewCategoryView.getText().toString();
-//            Toast.makeText(NewCategoryActivity.this, newCategory, Toast.LENGTH_LONG).show();
             mCategoryReference.push().setValue(newCategory);
         }
     }
